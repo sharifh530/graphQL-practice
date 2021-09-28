83dde3e0a109a54602864ba0008a1fc4f54b26d1
sharifh530 <sharifh530@gmail.com>
2021-09-28 15:47:10 +0600

    Created graphQL server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema.js");
+
+var app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("server started on port 3000"));
